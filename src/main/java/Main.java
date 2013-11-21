53d8846846eb7307f9db7a2caef35277c83adb87
robbyronk <robby.ronk@gmail.com>
2013-11-21 15:45:37 -0500

    Connect to Dropbox

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,19 +19,57 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Iterator;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.zip.GZIPOutputStream;
 
 import static org.apache.commons.io.CopyUtils.copy;
 
 public class Main {
-    {
-        final String APP_KEY = "123";
-        final String APP_SECRET = "456";
-    }
-
-    public static final String accessToken = "aaa";
     private static final String bucketName = "buckets";
     private static AmazonS3Client s3;
+
+    private static DbxClient connectToDropbox() {
+        Properties properties = new Properties();
+        try {
+            InputStream stream = Main.class.getResourceAsStream("dropbox.properties");
+            properties.load(stream);
+            stream.close();
+        } catch (IOException e) {
+            System.err.println("Unable to read dropbox.properties, can't continue. Sorry!");
+            throw new RuntimeException(e);
+        }
+
+        assert properties.stringPropertyNames().contains("app_key");
+        assert properties.stringPropertyNames().contains("app_secret");
+
+        DbxRequestConfig config = new DbxRequestConfig("dropbox-to-s3/0.1", Locale.getDefault().toString());
+        if (properties.stringPropertyNames().contains("accessToken")) {
+            return new DbxClient(config, properties.getProperty("accessToken"));
+        }
+
+        DbxAppInfo appInfo = new DbxAppInfo(properties.getProperty("app_key"), properties.getProperty("app_secret"));
+
+        DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+
+        // Have the user sign in and authorize your app.
+        String authorizeUrl = webAuth.start();
+        System.out.println("1. Go to: " + authorizeUrl);
+        System.out.println("2. Click \"Allow\" (you might have to log in first)");
+        System.out.println("3. Copy the authorization code.");
+        try {
+            String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+            String accessToken = webAuth.finish(code).accessToken;
+            System.out.println("Add `accessToken=" + accessToken + "` to your dropbox.properties");
+            return new DbxClient(config, accessToken);
+        } catch (IOException e) {
+            System.err.println("Unable to read the code from STDIN, just add the code to dropbox.properties.");
+            throw new RuntimeException(e);
+        } catch (DbxException e) {
+            System.err.println("Invalid authorization code. Try again...");
+            System.exit(1);
+            return null;
+        }
+    }
 
     public static void main(String[] args) throws IOException, DbxException {
 
@@ -39,25 +77,10 @@ public class Main {
 //
 //        s3.putObject(new PutObjectRequest("bucketname", key, inputStream, metadata));
 
-        DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
-//        DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
-//
-//        DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
-//
-//        // Have the user sign in and authorize your app.
-//        String authorizeUrl = webAuth.start();
-//        System.out.println("1. Go to: " + authorizeUrl);
-//        System.out.println("2. Click \"Allow\" (you might have to log in first)");
-//        System.out.println("3. Copy the authorization code.");
-//        String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-//
-//        // This will fail if the user enters an invalid authorization code.
-//        DbxAuthFinish authFinish = webAuth.finish(code);
 
-        DbxClient client = new DbxClient(config, accessToken);
+        DbxClient client = connectToDropbox();
 
         System.out.println("Linked account: " + client.getAccountInfo().displayName);
-        System.out.println("Access Token: " + client.getAccessToken());
 
         client.createFolder("/Test");
         DbxEntry.WithChildren listing = client.getMetadataWithChildren("/Test");
